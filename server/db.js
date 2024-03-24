b9dbb0d9f8496233fb30d5ffcd54f61e033701b2
bobae <bobae.choi2@gmail.com>
2024-03-24 16:31:49 -0700

    update proxy and db connection string

--- server/db.js
+++ server/db.js
@@ -1,10 +1,14 @@
 const Pool = require('pg').Pool;
+const connectionString = "postgresql://postgres:chlqhqo1@localhost:5432/paint_company";
+// const pool = new Pool({
+//     user: 'postgres',
+//     password: 'chlqhqo1',
+//     host: 'localhost',
+//     port: 5432,
+//     database: 'paint_company'
+// });
 const pool = new Pool({
-    user: 'postgres',
-    password: 'chlqhqo1',
-    host: 'localhost',
-    port: 5432,
-    database: 'paint_company'
-});
+    connectionString,
+})
 
 module.exports = pool;
